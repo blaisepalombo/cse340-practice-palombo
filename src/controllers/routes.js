8ae0c6dcf245064a7019bed035bfb6effb384a2a
Blaise Palombo <blaisepalombo@users.noreply.github.com>
2026-02-05 16:18:37 -0700

    Coding Challenge: Faculty Features

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -1,15 +1,16 @@
-// src/controllers/routes.js
-
 import { Router } from 'express';
 
 // Middleware
 import { addDemoHeaders } from '../middleware/demo/headers.js';
 
+// Page controllers
+import { homePage, aboutPage, demoPage, testErrorPage } from './index.js';
+
 // Catalog controllers
 import { catalogPage, courseDetailPage } from './catalog/catalog.js';
 
-// Basic page controllers
-import { homePage, aboutPage, demoPage, testErrorPage } from './index.js';
+// Faculty controllers
+import { facultyListPage, facultyDetailPage } from './faculty/faculty.js';
 
 const router = Router();
 
@@ -21,10 +22,14 @@ router.get('/about', aboutPage);
 router.get('/catalog', catalogPage);
 router.get('/catalog/:courseId', courseDetailPage);
 
-// Demo page with special middleware
+// Faculty directory routes
+router.get('/faculty', facultyListPage);
+router.get('/faculty/:facultyId', facultyDetailPage);
+
+// Demo page with route-specific middleware
 router.get('/demo', addDemoHeaders, demoPage);
 
-// Route to trigger a test error
+// Test error route
 router.get('/test-error', testErrorPage);
 
 export default router;
